752dc25cad813fcded6e5750102d9e448f65bf7f
亚豪 邱 <hao13770750068@outlook.com>
2022-04-28 15:35:33 +0800

    283. 移动零

---
+++ 283. 移动零.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+//
+//请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+//
+//
+//
+//示例 1:
+//
+//输入: nums = [0, 1, 0, 3, 12]
+//输出 : [1, 3, 12, 0, 0]
+
+/*
+    解一：两次遍历
+*/
+void moveZeroes(vector<int>& nums) {
+    int j = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        //把不等于0的元素依次和前面元素互换
+        if (nums[i] != 0) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    //把后面的元素置为0
+    for (int i = j; i < nums.size(); i++) {
+        nums[i] = 0;
+    }
+}
+
+
+int main() {
+    vector<int> v;
+    v.push_back(0);
+    v.push_back(0);
+    v.push_back(1);
+    moveZeroes(v);
+
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << endl;
+    }
+    //cout << m << endl;
+    return 0;
+}
--- 485. 最大连续 1 的个数.cpp
+++ 485. 最大连续 1 的个数.cpp
@@ -1,28 +1,28 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
-int findMaxConsecutiveOnes(vector<int>& nums) {
-    int result = 0, count = 0;
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] == 1) {
-            count++;
-        }
-        else {
-            result = max(result, count);
-            count = 0;
-        }
-    }
-    //c++可用max函数比较两个数大小
-    return max(result, count);
-}
-int main() {
-    vector<int> v;
-    v.push_back(1);
-    v.push_back(1);
-    v.push_back(0);
-    v.push_back(1);
-    int m = findMaxConsecutiveOnes(v);
-    cout << m << endl;
-    return 0;
-}
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//int findmaxconsecutiveones(vector<int>& nums) {
+//    int result = 0, count = 0;
+//    for (int i = 0; i < nums.size(); i++) {
+//        if (nums[i] == 1) {
+//            count++;
+//        }
+//        else {
+//            result = max(result, count);
+//            count = 0;
+//        }
+//    }
+//    //c++可用max函数比较两个数大小
+//    return max(result, count);
+//}
+//int main() {
+//    vector<int> v;
+//    v.push_back(1);
+//    v.push_back(1);
+//    v.push_back(0);
+//    v.push_back(1);
+//    int m = findmaxconsecutiveones(v);
+//    cout << m << endl;
+//    return 0;
+//}
